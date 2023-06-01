4259d7d0028e32e483c6b043ad2e775222763730
Nael Concescu <djanym@users.noreply.github.com>
2023-06-01 22:00:46 +0300

    adds react files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // Set the mode to development
+    entry: './src/js/index.js', // Change this path if your entry file is located elsewhere
+    output: {
+        filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'assets/js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    devtool: 'eval-source-map', // Add the devtool option for better debugging experience
+};
