4d35565c305a8893c47f4f5c267b2e74b5447a1e
Nael Concescu <djanym@users.noreply.github.com>
2023-06-04 22:30:03 +0300

    updates packages config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const { BundleStatsWebpackPlugin } = require('bundle-stats-webpack-plugin');
 
 module.exports = {
     mode: 'development', // Set the mode to development
     entry: './src/js/index.js', // Change this path if your entry file is located elsewhere
     output: {
-        filename: 'index.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'assets/js'),
     },
     module: {
@@ -16,7 +17,35 @@ module.exports = {
                     loader: 'babel-loader',
                 },
             },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
         ],
     },
+    plugins: [
+        new BundleStatsWebpackPlugin({
+            baseline: true,
+            html: false,
+            json: false,
+            outDir: '../',
+            stats: {
+                assets: true,
+                chunks: true,
+                modules: true,
+                builtAt: true,
+                hash: true,
+                moduleAssets: false,
+            },
+        }),
+    ],
     devtool: 'eval-source-map', // Add the devtool option for better debugging experience
+    stats: {
+        preset: 'summary',
+        assets: true,
+        chunks: true,
+        modules: true,
+        builtAt: true,
+        hash: true,
+    },
 };
